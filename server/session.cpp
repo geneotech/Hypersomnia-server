1a144d6529708f4c23e04702d7168f72e808e775
geneotech <unknownunreleased@gmail.com>
2014-06-05 19:41:53 +0200

    simple timeout checking

--- server/session.cpp
+++ server/session.cpp
@@ -4,7 +4,7 @@
 
 using namespace augs;
  
-session::session(unsigned num_of_workers) : main_worker_pool(num_of_workers) {}
+session::session(unsigned num_of_workers) : main_worker_pool(1) {}
 
 session::connection::connection(session& owner_session) : owner_session(owner_session) {}
 
@@ -18,26 +18,57 @@ void session::start_receiving_packets(int port) {
 	udp udp_socket;
 
 	if (udp_socket.open()) {
-		udp_socket.set_blocking(true);
+		udp_socket.set_blocking(false);
 
 		if (udp_socket.bind(port)) {
 			while (true) {
 				auto result = udp_socket.recv();
 				
 				if (result.result == network::io_result::SUCCESSFUL && result.bytes_transferred > 0) {
-					/* map the received packet to an existing connection or create a new one */
-					auto existing_or_new_connection = connections.emplace(result.sender_address.get_address_as_uint(), *this);
+					std::pair<std::unordered_map<unsigned long, connection>::iterator, bool> existing_or_new_connection;
+						
+					{
+						std::lock_guard<std::mutex> lock(connections_mutex);
+
+						/* map the received packet to an existing connection or create a new one */
+						existing_or_new_connection = connections.emplace(result.sender_address.get_address_as_uint(), *this);
+					}
 					
 					if (existing_or_new_connection.second) {
 						/* we have a new connection! */
-						// std::cout << "somebody connected." << std::endl;
+						 std::cout << "somebody connected." << std::endl;
 					}
 
-					/* dispatch connection-specific logic into worker thread */
+					(*existing_or_new_connection.first).second.last_received_packet.reset();
+
+					/* dispatch connection-specific logic into worker thread 
+					at this moment the iterator MUST be valid as only an incoming packet can stimulate connection removal
+					and packets come one-by-one only in this thread
+					or it may be timeout but timeout checks are only done in this thread after packet handling
+					*/
 					main_worker_pool.enqueue(std::bind(&session::connection::handle_packet, &(*existing_or_new_connection.first).second, result));
+
+					std::cout << "Bytes: " << result.bytes_transferred << "\nMessage: " << result.message.data.data() << std::endl;
 				}
-				
-				// std::cout << "Bytes: " << result.bytes_transferred << "\nMessage: " << result.message.data.data() << std::endl;
+
+				if (timeout_check_timer.get<std::chrono::milliseconds>() >= check_for_timeouts_every_ms) {
+					/* it is time to check for timeouts, lock connection state */
+					std::lock_guard<std::mutex> lock(connections_mutex);
+
+					std::vector<decltype(connections.begin())> connections_to_remove;
+
+					for (auto iter = connections.begin(); iter != connections.end(); ++iter) 
+						if ((*iter).second.request_removal || (*iter).second.last_received_packet.get<std::chrono::milliseconds>() > timeout_after_ms) {
+						connections_to_remove.push_back(iter);
+							std::cout << "Connection timed out." << std::endl;
+						} 
+
+					for (auto& iter : connections_to_remove) {
+						connections.erase(iter);
+					}
+
+					timeout_check_timer.reset();
+				}
 			}
 		}
 	}
--- server/session.h
+++ server/session.h
@@ -13,6 +13,8 @@ struct session {
 	struct connection {
 		augs::misc::timer last_received_packet;
 
+		bool request_removal = false;
+
 		/* handles incoming packet in the context of connection, executed in thread pool
 		whenever it needs to interact with some global, shared state, it enters an according critical section
 		*/
@@ -22,15 +24,19 @@ struct session {
 		connection(session&);
 	};
 
-	session() = default;
-	session(unsigned num_of_workers);
+	//session() = default;
+	session(unsigned num_of_workers = 1);
 
 	augs::network::udp socket;
 
 	resources::lua_state_wrapper global_lua_state;
 
+	double timeout_after_ms = 3000.0;
+	double check_for_timeouts_every_ms = 500.0;
 
-	std::mutex connection_vector_mutex;
+	augs::misc::timer timeout_check_timer;
+
+	std::mutex connections_mutex;
 
 	/* map ip address as uint to the connection structure */
 	std::unordered_map<unsigned long, connection> connections;
