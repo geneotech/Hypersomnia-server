5c3cd4ef00fcaf01672a6adc5fc554039e4f504c
geneotech <unknownunreleased@gmail.com>
2014-06-12 01:50:45 +0200

    successfully reproduced the demo in lua

--- main.cpp
+++ main.cpp
@@ -8,11 +8,6 @@
 
 using namespace augs;
 
-enum GameMessages
-{
-	ID_GAME_MESSAGE_1 = ID_USER_PACKET_ENUM + 1
-};
-
 int main() {
 	augs::global_log.open(L"engine_errors.txt");
 	
@@ -22,50 +17,6 @@ int main() {
 	lua_state.bind_whole_engine();
 
 	lua_state.dofile("init.lua"); 
-	 
-	network::network_interface server;
-	server.listen(37017, 2, 4);
-
-	network::network_interface::packet received;
-
-	while (1)
-	{ 
-		if (server.receive(received))
-		{
-			switch (received.byte(0))
-			{
-			case ID_NEW_INCOMING_CONNECTION:
-				printf("A connection is incoming.\n");
-				break;
-			case ID_DISCONNECTION_NOTIFICATION:
-				printf("A client has disconnected.\n");
-				break;
-			case ID_CONNECTION_LOST:
-				printf("A client lost the connection.\n");
-				break;
-
-			case ID_GAME_MESSAGE_1:
-			{
-				RakNet::RakString rs;
-				auto& bsIn = received.get_bitstream();
-				bsIn.IgnoreBytes(sizeof(RakNet::MessageID));
-				bsIn.Read(rs);
-				printf("%s\n", rs.C_String());
-
-
-				RakNet::BitStream bsOut;
-				bsOut.Write((RakNet::MessageID)ID_GAME_MESSAGE_1);
-				bsOut.Write("Hello world");
-				server.send(bsOut, HIGH_PRIORITY, RELIABLE_ORDERED, 0, received.guid(), false);
-			}
-				break;
-
-			default:
-				printf("Message with identifier %i has arrived.\n", received.byte(0));
-				break;
-			}
-		}
-	}
 
 	framework::deinit();
 	return 0;
--- output/init.lua
+++ output/init.lua
@@ -1,1 +1,39 @@
 print "Initialization successful."
+
+ENGINE_DIRECTORY = "..\\..\\Augmentations\\scripts\\"
+dofile (ENGINE_DIRECTORY .. "load_libraries.lua")
+
+local server = network_interface()
+server:listen(37017, 2, 4)
+
+local received = network_packet()
+
+network_message.ID_GAME_MESSAGE_1 = network_message.ID_USER_PACKET_ENUM + 1
+
+while true do
+	if server:receive(received) then
+		local message_type = received:byte(0)
+		if message_type == network_message.ID_NEW_INCOMING_CONNECTION then
+			print "A connection is incoming."
+		elseif message_type == network_message.ID_DISCONNECTION_NOTIFICATION then
+			print "A client has disconnected."
+		elseif message_type == network_message.ID_CONNECTION_LOST then
+			print "A client lost the connection."
+		elseif message_type == network_message.ID_GAME_MESSAGE_1 then
+			rs = RakString();
+			local bsIn = received:get_bitstream()
+			bsIn:IgnoreBytes(1)
+			bsIn:ReadRakString(rs);
+			print (rs:C_String())
+
+			bsOut = BitStream()
+			bsOut:WriteByte(UnsignedChar(network_message.ID_GAME_MESSAGE_1));
+			WriteCString(bsOut, "Hello world");
+			server:send(bsOut, send_priority.HIGH_PRIORITY, send_reliability.RELIABLE_ORDERED, 0, received:guid(), false)
+		
+		
+		else
+			print ("Message with identifier " .. message_type .. " has arrived.")
+		end
+	end
+end
