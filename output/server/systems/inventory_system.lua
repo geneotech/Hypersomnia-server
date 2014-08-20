fa33fda1e436c770abdfe5bd263b3dbae2ebe614
geneotech <unknownunreleased@gmail.com>
2014-08-20 23:55:31 +0200

    pick request handling and sending wields

--- output/server/systems/inventory_system.lua
+++ output/server/systems/inventory_system.lua
@@ -4,19 +4,69 @@ function inventory_system:get_required_components()
 	return { "inventory" }
 end
 
-function inventory_system:handle_item_requests()
-	--local msgs = self.owner_entity_system.messages["PICK_ITEM_REQUEST"]
-	--
-	--for i=1, #msgs do
-	--	local msg = msgs[i]
-	--	local subject = msg.subject
-	--	local item = msg.item
-	--	
-	--	local inventory = subject.inventory
-	--	
-	--	inventory.available_items[item.replication.id] = item
-	--end
+function inventory_system:get_item_in_range(physics_system, what_entity, try_to_pick_weapon)
+	local items_in_range = physics_system:query_body(what_entity, filters.ITEM_PICK, nil)
+	
+	local found_item;
+	
+	for candidate in items_in_range.bodies do
+		if try_to_pick_weapon == nil or (try_to_pick_weapon ~= nil and body_to_entity(candidate) == try_to_pick_weapon) then 
+			found_item = body_to_entity(candidate).script
+			break
+		end
+	end
+	
+	return found_item
 end
+
+function inventory_system:handle_pick_requests(world_object)
+	local msgs = self.owner_entity_system.messages["PICK_ITEM_REQUEST"]
+	
+	for i=1, #msgs do
+		local msg = msgs[i]
+		local subject = msg.subject
+		local character = subject.client.controlled_object
+		
+		if character ~= nil then
+			local wield = character.wield
+			if wield ~= nil then
+				-- subject validity ensured here
+				local found_item = self:get_item_in_range(world_object.physics_system, character.cpp_entity)
+				
+				--
+				--if wield.wielded_item ~= nil then
+				--	self.owner_entity_system:post_table("wield_item", {
+				--		subject = character,
+				--		drop = true
+				--	})
+				--end
+			    --
+				if found_item then
+					print "found an item"
+					
+					self.owner_entity_system:post_table("pick_item", {
+						subject = character,
+						item = found_item
+					})
+				end
+			end
+		end
+	end
+end
+
+--function inventory_system:handle_item_requests()
+--	local msgs = self.owner_entity_system.messages["PICK_ITEM_REQUEST"]
+--	
+--	for i=1, #msgs do
+--		local msg = msgs[i]
+--		local subject = msg.subject
+--		local item = msg.item
+--		
+--		local inventory = subject.inventory
+--		
+--		inventory.available_items[item.replication.id] = item
+--	end
+--end
 
 
 function inventory_system:translate_item_events()
@@ -27,73 +77,49 @@ function inventory_system:translate_item_events()
 		local subject = msg.subject
 		local item = msg.item
 		
-		local inventory = subject.inventory
-		
-		-- don't longer replicate it in the public space
-		item.item:pick()
-		item.item.owner_inventory = subject
-		inventory.available_items[item.replication.id] = item
-		
-		-- not need for any kind of notification - unreplicated object will simply be deleted remotely
+		local inventory_subject = subject.wield.wielded_items[components.wield.keys.INVENTORY]
+		local inventory = inventory_subject.inventory
+	
+		self.owner_entity_system:post_table("item_wielder_change", {
+			subject = inventory_subject,
+			item = msg.item,
+			wield = true,
+			wielding_key = item.replication.id,
+			wield_if_unwielded = true
+		})
 	end
 	
-	msgs = self.owner_entity_system.messages["drop_item"]
-	
-	for i=1, #msgs do
-		local msg = msgs[i]
-		local item = msg.item
-		
-		local wielder = item.item.wielder
-		
-		if wielder then
-			self.owner_entity_system:post_table("wield_item", {
-				subject = wielder,
-				item = nil
-			})
-		end
-		
-		if msg.owner_inventory then
-			msg.owner_inventory.inventory[item.replication.id] = nil
-		end
-		
-		item.item:drop()
-	end
+	--msgs = self.owner_entity_system.messages["drop_item"]
+	--
+	--for i=1, #msgs do
+	--	local msg = msgs[i]
+	--	local item = msg.item
+	--	
+	--	local wielder = item.item.wielder
+	--	
+	--	if wielder then
+	--		self.owner_entity_system:post_table("wield_item", {
+	--			subject = wielder,
+	--			item = nil
+	--		})
+	--	end
+	--	
+	--	if msg.owner_inventory then
+	--		msg.owner_inventory.inventory[item.replication.id] = nil
+	--	end
+	--	
+	--	item.item:drop()
+	--end
 end
 
 
-function inventory_system:handle_drop_requests()
-	local msgs = self.owner_entity_system.messages["drop_item"]
-	
-	for i=1, #msgs do
-		local msg = msgs[i]
-		local item = msg.item
-		
-		item.item:drop()
-	end
-end
-
-
-function inventory_system:broadcast_item_events()
-	msgs = self.owner_entity_system.messages["drop_item"]
-	
-	for i=1, #msgs do
-		local msg = msgs[i]
-		local item = msg.item
-		
-		local wielder = item.item.wielder
-		
-		if wielder ~= nil then
-			wielder.replication.sub_entities["ITEM_ENTITY"] = nil
-							
-			if wielder.client_controller ~= nil then
-				item.replication:clear_group_for_client(wielder.client_controller.owner_client)
-			end
-		end
-		
-		item.replication:switch_public_group("DROPPED_PUBLIC")
-		
-		-- the item will be replicated on the next update as DROPPED_PUBLIC.
-		-- so we don't need to tell the clients about the DROP itself,
-		-- as they don't have to know where does the item come from.
-	end
-end
+--function inventory_system:handle_drop_requests()
+--	local msgs = self.owner_entity_system.messages["drop_item"]
+--	
+--	for i=1, #msgs do
+--		local msg = msgs[i]
+--		local item = msg.item
+--		
+--		item.item:drop()
+--	end
+--end
