7b910efdf84c6fd2d2813944e496021186aec5bf
geneotech <unknownunreleased@gmail.com>
2014-08-31 02:49:39 +0200

    moved session creation code to a separate file

--- output/server/server_class.lua
+++ output/server/server_class.lua
@@ -44,6 +44,7 @@ dofile "server\\systems\\bullet_broadcast_system.lua"
 dofile "server\\chat.lua"
 
 server_class = inherits_from()
+dofile "server\\session_creation.lua"
 
 function server_class:constructor()
 	self.server = network_interface()
@@ -132,131 +133,12 @@ function server_class:set_current_map(map_filename, loader_filename)
 	end)
 end
 
-function server_class:new_client(new_guid)
-	local world_character = world_archetypes.create_player(self.current_map, self.current_map.teleport_shuffler:next_value().pos)
-	
-	local public_character_modules = create_replica { "movement", "crosshair", "health" }
-	local owner_character_modules = create_replica { "movement", "health" }
-	
-	local inventory_modules = create_replica { "item" }
-	
-	--local client_modules = {}
-	--client_modules["client_info"] = replication_module:create(protocol.replication_tables.client_info)
-	
-	local new_client = components.create_components {
-		client = {
-			guid = new_guid
-		}
-		
-		--client_info = {},
-		--
-		--replication = {
-		--	module_sets = {
-		--		PUBLIC = {
-		--			replica = client_modules,
-		--			archetype_name = "CLIENT_INFO"
-		--		}
-		--	}
-		--}
-	}
-
-	local new_controlled_character = components.create_components {
-		client_controller = {
-			owner_client = new_client
-		},
-		
-		cpp_entity = world_character,
-		
-		replication = {
-			module_sets = {
-				PUBLIC = {
-					replica = public_character_modules,
-					archetype_name = "REMOTE_PLAYER"
-				},
-					
-				OWNER = {
-					replica = owner_character_modules,
-					archetype_name = "CONTROLLED_PLAYER"
-				}
-			}
-		},
-		
-		health = {
-			hp = 100
-		},
-		
-		orientation = {},
-		
-		wield = {}
-	}
-	
-	local new_character_inventory = components.create_components {
-		replication = {
-			module_sets = {
-				OWNER = {
-					replica = inventory_modules,
-					archetype_name = "INVENTORY" 
-				}
-			}
-		},
-		
-		inventory = {},
-		
-		item = {},
-		
-		wield = {}
-	}
-	
-	self.entity_system_instance:add_entity(new_client)
-	self.entity_system_instance:add_entity(new_controlled_character)
-	world_archetypes.spawn_gun(self, "m4a1", world_character.transform.current.pos)
-	world_archetypes.spawn_gun(self, "shotgun", world_character.transform.current.pos)
-	self.entity_system_instance:add_entity(new_character_inventory)
-	
-	new_client.client.controlled_object = new_controlled_character
-	
-	self.entity_system_instance:post_table("item_wielder_change", { 
-		wield = true,
-		subject = new_controlled_character,
-		item = new_character_inventory,
-		wielding_key = components.wield.keys.INVENTORY
+function server_class:new_connection(new_guid)
+	self.user_map:add(new_guid, {
+		connection_duration = timer()
 	})
 	
-	--self.entity_system_instance:post_table("pick_item", { 
-	--	subject = new_controlled_character,
-	--	item = new_gun
-	--})			
-	
-	new_client.client.group_by_id[new_controlled_character.replication.id] = "OWNER"
-	
-	new_controlled_character.wield.on_item_unwielded = function (subject, dropped_item)
-		if dropped_item.cpp_entity.physics == nil then return end
-		
-		local body = dropped_item.cpp_entity.physics.body
-		local force = (subject.orientation.crosshair_position):normalize() * 100
-		
-		if subject.orientation.crosshair_position:length() < 0.01 then
-			force = vec2(100, 0)
-		end
-		
-		body:ApplyLinearImpulse(to_meters(force), body:GetWorldCenter(), true)
-		body:ApplyAngularImpulse(4, true)
-	end
-	
-	new_controlled_character.health.on_death = function(this)
-		--self.entity_system_instance:post_remove(this)
-		this.replication:broadcast_reliable(protocol.write_msg("DAMAGE_MESSAGE", {
-					victim_id = this.replication.id,
-					amount = this.health.hp - 100
-				}))
-		this.health.hp = 100
-				
-		this.cpp_entity.physics.body:SetTransform(to_meters(self.current_map.teleport_shuffler:next_value().pos), 0)
-	end
-	
-	self.user_map:add(new_guid, new_client)
-	
-	print "New client connected."
+	print "New incoming connection."
 end
 
 function server_class:remove_client(guid)	
@@ -285,18 +167,25 @@ function server_class:loop()
 		local is_reliable_transmission = message_type == protocol.RELIABLE_TRANSMISSION
 		
 		if message_type == network_event.ID_NEW_INCOMING_CONNECTION then
-			self:new_client(guid)
+			self:new_connection(guid)
 		elseif message_type == network_event.ID_DISCONNECTION_NOTIFICATION then
 			self:remove_client(guid)
 		elseif message_type == network_event.ID_CONNECTION_LOST then
 			self:remove_client(guid)
 		elseif message_type == protocol.GAME_TRANSMISSION or is_reliable_transmission then
-			local entity = self.user_map:at(guid) 
+			local entity = self.user_map:at(guid)
+			local target_channel;
+			
+			if entity.client then
+				target_channel = entity.client.net_channel
+			end
+			
 			self.entity_system_instance:post_table("network_message", { 
+				["guid"] = guid,
 				subject = entity,
 				data = packet,
-				["is_reliable_transmission"] = is_reliable_transmission,
-				channel = entity.client.net_channel
+				channel = target_channel,
+				["is_reliable_transmission"] = is_reliable_transmission
 			})
 		end
 	end
@@ -335,6 +224,7 @@ function server_class:loop()
 	self.systems.bullet_broadcast:handle_hit_requests()
 	
 	broadcast_chat_messages(self.entity_system_instance)
+	self:create_incoming_sessions()
 	
 	self.entity_system_instance:flush_messages()
 	
---
+++ output/server/session_creation.lua
@@ -0,0 +1,119 @@
+function server_class:create_incoming_sessions()
+	local msgs = self.entity_system_instance.messages["BEGIN_SESSION"]
+	
+	for i=1, #msgs do
+		local msg = msgs[i]
+		local new_guid = msg.guid
+	
+		local world_character = world_archetypes.create_player(self.current_map, self.current_map.teleport_shuffler:next_value().pos)
+		
+		local public_character_modules = create_replica { "movement", "crosshair", "health" }
+		local owner_character_modules = create_replica { "movement", "health" }
+		
+		local inventory_modules = create_replica { "item" }
+		
+		local new_client = components.create_components {
+			client = {
+				guid = new_guid
+			}
+		}
+	
+		local new_controlled_character = components.create_components {
+			client_controller = {
+				owner_client = new_client
+			},
+			
+			cpp_entity = world_character,
+			
+			replication = {
+				module_sets = {
+					PUBLIC = {
+						replica = public_character_modules,
+						archetype_name = "REMOTE_PLAYER"
+					},
+						
+					OWNER = {
+						replica = owner_character_modules,
+						archetype_name = "CONTROLLED_PLAYER"
+					}
+				}
+			},
+			
+			health = {
+				hp = 100
+			},
+			
+			orientation = {},
+			
+			wield = {}
+		}
+		
+		local new_character_inventory = components.create_components {
+			replication = {
+				module_sets = {
+					OWNER = {
+						replica = inventory_modules,
+						archetype_name = "INVENTORY" 
+					}
+				}
+			},
+			
+			inventory = {},
+			
+			item = {},
+			
+			wield = {}
+		}
+		
+		self.entity_system_instance:add_entity(new_client)
+		self.entity_system_instance:add_entity(new_controlled_character)
+		world_archetypes.spawn_gun(self, "m4a1", world_character.transform.current.pos)
+		world_archetypes.spawn_gun(self, "shotgun", world_character.transform.current.pos)
+		self.entity_system_instance:add_entity(new_character_inventory)
+		
+		new_client.client.controlled_object = new_controlled_character
+		
+		self.entity_system_instance:post_table("item_wielder_change", { 
+			wield = true,
+			subject = new_controlled_character,
+			item = new_character_inventory,
+			wielding_key = components.wield.keys.INVENTORY
+		})
+		
+		--self.entity_system_instance:post_table("pick_item", { 
+		--	subject = new_controlled_character,
+		--	item = new_gun
+		--})			
+		
+		new_client.client.group_by_id[new_controlled_character.replication.id] = "OWNER"
+		
+		new_controlled_character.wield.on_item_unwielded = function (subject, dropped_item)
+			if dropped_item.cpp_entity.physics == nil then return end
+			
+			local body = dropped_item.cpp_entity.physics.body
+			local force = (subject.orientation.crosshair_position):normalize() * 100
+			
+			if subject.orientation.crosshair_position:length() < 0.01 then
+				force = vec2(100, 0)
+			end
+			
+			body:ApplyLinearImpulse(to_meters(force), body:GetWorldCenter(), true)
+			body:ApplyAngularImpulse(4, true)
+		end
+		
+		new_controlled_character.health.on_death = function(this)
+			--self.entity_system_instance:post_remove(this)
+			this.replication:broadcast_reliable(protocol.write_msg("DAMAGE_MESSAGE", {
+						victim_id = this.replication.id,
+						amount = this.health.hp - 100
+					}))
+			this.health.hp = 100
+					
+			this.cpp_entity.physics.body:SetTransform(to_meters(self.current_map.teleport_shuffler:next_value().pos), 0)
+		end
+		
+		self.user_map:add(new_guid, new_client)
+		
+		print "New client connected."
+	end
+end
