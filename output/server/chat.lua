1f096cebc7a846ead1d10d1acd6063a6a0df68ba
geneotech <unknownunreleased@gmail.com>
2014-09-04 04:36:14 +0200

    sending character id with chat message

--- output/server/chat.lua
+++ output/server/chat.lua
@@ -6,10 +6,19 @@ function broadcast_chat_messages(owner_entity_system)
 		local msg = msgs[i]
 		
 		for j=1, #client_sys.targets do
-			client_sys.network:send(protocol.make_reliable_bs(protocol.write_msg("REMOTE_CHAT_MESSAGE", {
-					nickname = msg.subject.client.nickname,
-					message = msg.data.message
-				})), send_priority.IMMEDIATE_PRIORITY, send_reliability.RELIABLE_ORDERED, 0, client_sys.targets[j].client.guid, false)
+			local client = client_sys.targets[j]
+			
+			local out_msg = {
+				nickname = msg.subject.client.nickname,
+				message = msg.data.message 
+			}
+			
+			if client.client.controlled_object then
+				out_msg.optional_object_id = client.client.controlled_object.replication.id
+			end
+		
+			client_sys.network:send(protocol.make_reliable_bs(protocol.write_msg("REMOTE_CHAT_MESSAGE", out_msg)), 
+				send_priority.IMMEDIATE_PRIORITY, send_reliability.RELIABLE_ORDERED, 0, client.client.guid, false)
 		end
 	end
 end
