40635454d871d6394e2d18da633e59ec3723474f
geneotech <unknownunreleased@gmail.com>
2014-09-10 15:37:03 +0200

    calling remote scripts

--- output/server/chat.lua
+++ output/server/chat.lua
@@ -1,3 +1,18 @@
+function clients(script_str)
+	local client_sys = server.systems.client
+	
+	for j=1, #client_sys.targets do
+		local client = client_sys.targets[j]
+		
+		local out_msg = {
+			script = script_str
+		}
+	
+		client_sys.network:send(protocol.make_reliable_bs(protocol.write_msg("REMOTE_COMMANDS", out_msg)), 
+			send_priority.IMMEDIATE_PRIORITY, send_reliability.RELIABLE_ORDERED, 0, client.client.guid, false)
+	end
+end
+
 function broadcast_chat_messages(owner_entity_system)
 	local msgs = owner_entity_system.messages["CHAT_MESSAGE"]
 	local client_sys = owner_entity_system.all_systems.client
@@ -5,20 +20,31 @@ function broadcast_chat_messages(owner_entity_system)
 	for i=1, #msgs do
 		local msg = msgs[i]
 		
-		for j=1, #client_sys.targets do
-			local client = client_sys.targets[j]
-			
-			local out_msg = {
-				nickname = msg.subject.client.nickname,
-				message = msg.data.message 
-			}
-			
-			if client.client.controlled_object then
-				out_msg.optional_object_id = client.client.controlled_object.replication.id
+		if wstr_eq_n(towchar_vec("!call"), msg.data.message, string.len("!call")) then
+			print "COMMAND CALLED!"
+			local outstr = string.sub (wchar_vec_to_str(msg.data.message), 6)
+			print (outstr)
+			local compiled_script = loadstring (outstr)
+			if compiled_script then 
+				local status, err = pcall(function() compiled_script () end) 
+				print(err) 
 			end
-		
-			client_sys.network:send(protocol.make_reliable_bs(protocol.write_msg("REMOTE_CHAT_MESSAGE", out_msg)), 
-				send_priority.IMMEDIATE_PRIORITY, send_reliability.RELIABLE_ORDERED, 0, client.client.guid, false)
+		else
+			for j=1, #client_sys.targets do
+				local client = client_sys.targets[j]
+				
+				local out_msg = {
+					nickname = msg.subject.client.nickname,
+					message = msg.data.message 
+				}
+				
+				if client.client.controlled_object then
+					out_msg.optional_object_id = client.client.controlled_object.replication.id
+				end
+			
+				client_sys.network:send(protocol.make_reliable_bs(protocol.write_msg("REMOTE_CHAT_MESSAGE", out_msg)), 
+					send_priority.IMMEDIATE_PRIORITY, send_reliability.RELIABLE_ORDERED, 0, client.client.guid, false)
+			end
 		end
 	end
 end
